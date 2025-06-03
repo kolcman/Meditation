44a23eeeddc165d934629dd3bbdaf72c84c2ac90
kolcman <m.kolcman@yandex.ru>
2025-06-03 14:13:13 +0300

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
-import IconLogo from './components/icon/IconLogo.vue';
 
 
 </script>
 
 <template>
   <main>
-    <div class="login">
-      Simple text
-      <IconLogo />
-    </div>
+
   </main>
 </template>
 
-<style scoped>
-.login {
-  background-image: url("/public/bg.jpg");
-}
-</style>
+<style scoped></style>
