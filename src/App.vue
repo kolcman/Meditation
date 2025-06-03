e1f880e31466f249f3891b23a6390aec72b77058
kolcman <m.kolcman@yandex.ru>
2025-06-03 14:06:28 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import IconLogo from './components/icon/IconLogo.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <div class="login">
+      Simple text
+      <IconLogo />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.login {
+  background-image: url("/public/bg.jpg");
+}
+</style>
