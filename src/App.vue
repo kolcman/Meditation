492d24b82bdad0687c4113bc5922004769fb611b
kolcman <m.kolcman@yandex.ru>
2025-06-05 09:15:00 +0300

    Добавил:
    - Зависимости для Pinia и Axios
    - Сверстал компонент карточки медитации.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 
 
+import MeditationCard from '@/components/MeditationCard.vue';
 </script>
 
 <template>
   <main>
-
+    <ul class="meditation-list">
+      <li><MeditationCard/></li>
+    </ul>
   </main>
 </template>
 
---
+++ src/components/icon/ButtonBase.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">
+    <slot />
+    <div>
+      <slot name='icon'/>
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  background: var(--color-bg);
+  color: var(--color-fg);
+  font-family: var(--font),serif;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+}
+</style>
